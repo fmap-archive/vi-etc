876b2bdb1bad22fac03d784d2e667a8efd5f3065
vi <noreply@example.org>
2013-12-08 13:43:55 +0800

    hjkl: {g,GG}

--- .surf/script/hjkl.js
+++ .surf/script/hjkl.js
@@ -1,5 +1,6 @@
 var ignoreOn = [];
 var distance = 30;
+var keyQueue = [];
 
 function scroll (axis, dt) {
   switch (axis) {
@@ -27,13 +28,17 @@ function butIgnored (fn) {
   butBlacklist(ignoreOn, fn);
 }
 
-function translateKey (code) {
-  switch (code) {
-    case 72: return 'h'
-    case 74: return 'j'
-    case 75: return 'k'
-    case 76: return 'l'
+function queueKey(ev) {
+  var chr;
+  switch (ev.keyCode) {
+    case 71: chr = 'g'; break;
+    case 72: chr = 'h'; break;
+    case 74: chr = 'j'; break;
+    case 75: chr = 'k'; break;
+    case 76: chr = 'l'; break;
   };
+  keyQueue[0] = keyQueue[1];
+  keyQueue[1] = ev.shiftKey ? chr.toUpperCase() : chr;
 };
 
 function butIgnoredScroll(axis, dt) {
@@ -42,15 +47,17 @@ function butIgnoredScroll(axis, dt) {
 };
 
 function processKey (ev) {
-  var key = translateKey(ev.keyCode);
-  switch (key) {
-    case 'h': butIgnoredScroll('X', -distance); break;
-    case 'j': butIgnoredScroll('Y',  distance); break;
-    case 'k': butIgnoredScroll('Y', -distance); break;
-    case 'l': butIgnoredScroll('X',  distance); break;
+  var key = queueKey(ev);
+  switch (keyQueue[1]) {
+    case 'h':  butIgnoredScroll('X', -distance);        break;
+    case 'j':  butIgnoredScroll('Y',  distance);        break;
+    case 'k':  butIgnoredScroll('Y', -distance);        break;
+    case 'l':  butIgnoredScroll('X',  distance);        break;
+    case 'G':  butIgnoredScroll('Y',  document.height); break;
+    case 'g':  if (keyQueue[0]=='g') {
+      butIgnoredScroll('Y', -document.height); break; 
+    }
   };
 }
 
-window.addEventListener('DOMContentLoaded', function() {
-  document.addEventListener('keydown', processKey, false);
-}, true);
+document.addEventListener('keydown', processKey, false);
