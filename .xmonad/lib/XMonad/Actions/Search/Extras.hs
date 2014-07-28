e26ce7618b4dde70d0dd1e6f746a03e000bc9f9d
vi <me@vikramverma.com>
2014-07-28 19:30:57 +0800

    XMonad: use my DuckDuckGo settings.

--- .xmonad/lib/XMonad/Actions/Search/Extras.hs
+++ .xmonad/lib/XMonad/Actions/Search/Extras.hs
@@ -27,7 +27,7 @@ instance Show Browser where
   show Browser{..} = [i|#{executable} #{intercalate " " options}|]
 
 ddg :: Site
-ddg = ("https://duckduckgo.com?q="++)
+ddg = ("https://duckduckgo.com/?kp=-1&kz=-1&k1=-1&kw=s&ks=n&kae=-1&kaf=1&kac=-1&kf=-1&ko=-2&q=" ++)
 
 search :: Browser -> Site -> Query -> X ()
 search Browser{..} site query = safeSpawn executable $ options ++ [site query]
